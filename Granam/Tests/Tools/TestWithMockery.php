2d6f0485ebcec8f4bd0c4c75afd465e622fa1a3e
Jaroslav Týc <mail@jaroslavtyc.com>
2016-12-07 10:51:08 +0100

    Test with mockery can simpler way add type expectation and give current SUT class

--- Granam/Tests/Tools/TestWithMockery.php
+++ Granam/Tests/Tools/TestWithMockery.php
@@ -21,4 +21,40 @@ abstract class TestWithMockery extends \PHPUnit_Framework_TestCase
 
         return \Mockery::mock($className);
     }
+
+    /**
+     * @param mixed $expected
+     * @return \Mockery\Matcher\Type
+     */
+    protected function type($expected)
+    {
+        return \Mockery::type($this->getTypeOf($expected));
+    }
+
+    /**
+     * @param $value
+     * @return string
+     */
+    private function getTypeOf($value)
+    {
+        if (is_string($value)) {
+            return $value; // not type of "string" but direct description - like class name
+        }
+        if (is_object($value)) {
+            return get_class($value);
+        }
+
+        return gettype($value);
+    }
+
+    /**
+     * Expects test class with name \Granam\Tests\Tools\TestWithMockery therefore extended by \Tests sub-namespace
+     * and Test suffix
+     *
+     * @return string|TestWithMockery
+     */
+    protected function getSutClass()
+    {
+        return preg_replace('~\\\Tests(.+)Test$~', '$1', static::class);
+    }
 }
